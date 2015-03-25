dd77b484049955ec60e27e4e570b27a5d3180e9b
Benot Gidon <gidon@intechinfo.fr>
2015-03-25 15:52:03 +0100

    Created the solution with the window
    Added the different nugets packages
    Initialized all needed functions with requirements

---
+++ PoCYodiiScript/PoCYodiiScript/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System.Windows;
+
+namespace PoCYodiiScript
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void add_Click( object sender, RoutedEventArgs e )
+        {
+            //todo Ironpython
+        }
+
+        private void rem_Click( object sender, RoutedEventArgs e )
+        {
+            //todo IronRuby
+        }
+
+        private void mul_Click( object sender, RoutedEventArgs e )
+        {
+            //TODO LUA
+        }
+
+        private void div_Click( object sender, RoutedEventArgs e )
+        {
+            //todo VB
+        }
+
+        private void pow_Click( object sender, RoutedEventArgs e )
+        {
+            //todo JS
+        }
+    }
+}
