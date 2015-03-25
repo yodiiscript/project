57c03f74d98c0b0789c95e021f75fefed7d42b79
Nattefrost <escande.d@gmail.com>
2015-03-25 16:21:19 +0100

    + et - respectivement ironpython et lua

--- PoCYodiiScript/PoCYodiiScript/MainWindow.xaml.cs
+++ PoCYodiiScript/PoCYodiiScript/MainWindow.xaml.cs
@@ -1,4 +1,9 @@
 ﻿using System.Windows;
+using System;
+using DynamicLua;
+using IronPython;
+using System.Text;
+
 
 namespace PoCYodiiScript
 {
@@ -14,7 +19,16 @@ namespace PoCYodiiScript
 
         private void add_Click( object sender, RoutedEventArgs e )
         {
-            //todo Ironpython
+            var n1 =  Convert.ToInt32(this.input1.Text);
+            var n2 =  Convert.ToInt32(this.input2.Text);
+            var pySrc = @"def add(a,b):
+                            return a+b";
+            var engine = IronPython.Hosting.Python.CreateEngine();
+            var scope  = engine.CreateScope();
+            engine.Execute( pySrc, scope );
+            var add =  scope.GetVariable( "add" );
+            var sum = add( n1, n2 );
+            this.result.Text =  sum.ToString();
         }
 
         private void rem_Click( object sender, RoutedEventArgs e )
@@ -24,7 +38,13 @@ namespace PoCYodiiScript
 
         private void mul_Click( object sender, RoutedEventArgs e )
         {
-            //TODO LUA
+            dynamic lua = new DynamicLua.DynamicLua();
+            string luaInput = System.IO.File.ReadAllText( "../../add.lua" );
+            lua( luaInput );
+            int n1 = Convert.ToInt32( this.input1.Text );
+            int n2 = Convert.ToInt32( this.input2.Text );
+            dynamic r = lua.multiply( n1, n2 );
+            this.result.Text = r.ToString();
         }
 
         private void div_Click( object sender, RoutedEventArgs e )
