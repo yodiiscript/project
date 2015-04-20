0b1c704e984e86fc66934f95625d56758b091300
Nattefrost <escande.d@gmail.com>
2015-04-20 15:04:38 +0200

    Properly redone IronPython + and -

--- PoCYodiiScript/PoCYodiiScript/MainWindow.xaml.cs
+++ PoCYodiiScript/PoCYodiiScript/MainWindow.xaml.cs
@@ -2,6 +2,8 @@
 using System;
 using DynamicLua;
 using IronPython;
+using IronPython.Hosting;
+using Microsoft.Scripting.Hosting;
 using System.Text;
 using ActiveXScriptLib;
 using System.Runtime.InteropServices;
@@ -30,27 +32,26 @@ namespace PoCYodiiScript
 
         private void add_Click( object sender, RoutedEventArgs e )
         {
-            /*input.Value1 = input1.Text;
-            input.Value2 = input2.Text ;
-
-
-            var n1 =  Convert.ToInt32(this.input1.Text);
-            var n2 =  Convert.ToInt32(this.input2.Text);
-            var pySrc = @"def add(a,b):
-                            return a+b";
-            var engine = IronPython.Hosting.Python.CreateEngine();
-            var scope  = engine.CreateScope();
+            ScriptEngine engine = Python.CreateEngine();
+            ScriptScope scope = engine.CreateScope();
+            this.input.Value1 = this.input1.Text;
+            this.input.Value2 = this.input2.Text;
+            scope.SetVariable( "input", this.input );
+            scope.SetVariable( "output", this.output );
+            string pySrc =   "output.SetResult(str(int(input.Value1)+int(input.Value2)))";
             engine.Execute( pySrc, scope );
-            var add =  scope.GetVariable( "add" );
-            var sum = add( n1, n2 );
-            this.result.Content =  sum.ToString();*/
         }
 
         private void rem_Click( object sender, RoutedEventArgs e )
         {
-            input.Value1 = input1.Text;
-            input.Value2 = input2.Text;
-            //todo soustract 
+            ScriptEngine engine = Python.CreateEngine();
+            ScriptScope scope = engine.CreateScope();
+            this.input.Value1 = this.input1.Text;
+            this.input.Value2 = this.input2.Text;
+            scope.SetVariable( "input", this.input );
+            scope.SetVariable( "output", this.output );
+            string pySrc =   "output.SetResult(str(int(input.Value1)-int(input.Value2)))";
+            engine.Execute( pySrc, scope );
         }
 
         private void mul_Click( object sender, RoutedEventArgs e )
