4821d24e41ae0d202ca0e435e9863cafa5e4de0b
MorganFR <lartiguemorgan@hotmail.com>
2015-05-06 14:12:31 +0200

    added script class

---
+++ Yodii-script.IDE/Yodii-script.IDE/View Models/Script.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Yodii_script.IDE.View_Models
+{
+    class Script
+    {
+        public String name { get; set; }
+        public String language { get; set; }
+        public String description { get; set; }
+        public String content { get; set; }  
+    }
+}
